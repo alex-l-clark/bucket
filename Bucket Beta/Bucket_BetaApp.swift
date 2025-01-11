b4dd2cb493e5f800a673658791a320f436809535
Alex Clark <99.alexclark@gmail.com>
2025-01-11 18:25:03 -0500

    Initial Commit

---
+++ Bucket Beta/Bucket_BetaApp.swift
@@ -0,0 +1,17 @@
+//
+//  Bucket_BetaApp.swift
+//  Bucket Beta
+//
+//  Created by Alex Clark on 1/11/25.
+//
+
+import SwiftUI
+
+@main
+struct Bucket_BetaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Bucket BetaTests/Bucket_BetaTests.swift
@@ -0,0 +1,17 @@
+//
+//  Bucket_BetaTests.swift
+//  Bucket BetaTests
+//
+//  Created by Alex Clark on 1/11/25.
+//
+
+import Testing
+@testable import Bucket_Beta
+
+struct Bucket_BetaTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
